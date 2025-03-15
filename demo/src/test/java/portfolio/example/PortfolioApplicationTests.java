2f6f0b326392f624b123b9a758bc64126549a9a9
たぼヴ <taboon.taboboon@gmail.com>
2025-03-16 00:48:52 +0900

    ポートフォリオの環境構築

---
+++ demo/src/test/java/portfolio/example/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
